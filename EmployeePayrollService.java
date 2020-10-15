1b00529a499eddd2c89ad178e59712b34df76b88
premchandAllu <premchandallu1999@gmail.com>
2020-10-15 16:02:53 +0530

    Added :Welcome to Employee Payroll Service

---
+++ EmployeePayrollService.java
@@ -0,0 +1,11 @@
+package com.blz.EmployeePayrollService;
+
+public class EmployeePayrollService {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee Payroll Service using IO Streams");
+
+	}
+
+}
